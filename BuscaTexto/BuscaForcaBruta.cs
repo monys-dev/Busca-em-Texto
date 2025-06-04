3db7eff4b25cb16aa6943361b8e9df4720482124
monys-dev <ramonys.contato@gmail.com>
2025-06-03 21:33:04 -0300

    Adicionar arquivos de projeto.

---
+++ BuscaTexto/BuscaForcaBruta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscaTexto {
+    class BuscaForcaBruta {
+        public static int forcaBruta(String p, String t) {
+            int i, j, aux;
+            int m = p.Length;
+            int n = t.Length;
+            for (i = 0; i < n; i++) {
+                aux = i;
+                for (j = 0; j < m && aux < n; j++) {
+                    if (t[aux] != p[j])
+                        break;
+                    aux++;
+                }
+                if (j == m)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ BuscaTexto/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscaTexto {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void novoToolStripMenuItem_Click(object sender, EventArgs e) {
+            texto.Text = "";
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e) {
+            // TODO
+            // Complete com seu nome e código de matrícula
+            MessageBox.Show(this,
+               "Busca em Texto - 2025/1\n\nDesenvolvido por:\n79999999 - NOME DO ALUNO\nProf. Virgílio Borges de Oliveira\n\nAlgoritmos e Estruturas de Dados II\nFaculdade COTEMIG\nSomente para fins didáticos.",
+               "Sobre o trabalho...",
+               MessageBoxButtons.OK,
+               MessageBoxIcon.Information);
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e) {
+            // TODO
+            // Caixa de diálogo de abrir arquivo com filtro para extensão txt e rtf
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+    }
+}
